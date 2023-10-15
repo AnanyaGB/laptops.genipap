24bdda89632897ac1472d48f10dc39406518b1cd
Ananya Gupta Bhaya <ananyaguptabhaya@outlook.com>
2023-10-15 16:48:12 +0530

    update

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Navbar from "./navbar";
+import NextNProgress from "nextjs-progressbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative">
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Navbar />
       <div className="min-h-screen">{children}</div>
     </div>
